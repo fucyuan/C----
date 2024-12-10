5861a6998eb66b4b52006cf397660a57c142b4ac
“fucyuan” <194317428@qq.com>
2024-12-10 17:10:59 +0800

    解决迭代器失效的问题，需要在每次迭代之前都重新生成一个新的迭代器。

--- Vector/include/Vector.h
+++ Vector/include/Vector.h
@@ -1,157 +1,205 @@
 #ifndef VECTOR_H
 #define VECTOR_H
-#include"Allocator.h"
+
+#include "Allocator.h"
 #include <iostream>
-template<typename T,typename Allocator = Allocator<T>>
-/*容器底层内存开辟和释放由Allocator类负责*/
+#include <stdexcept>
+
+template <typename T, typename Allocator = Allocator<T>>
 class Vector {
+public:
+    Vector(int sz = 10, Allocator allocator = Allocator())
+        : first(nullptr), last(nullptr), end_of_storage(nullptr), alloc(allocator), head(new iterator_base()) {
+        first = alloc.allocate(sz); // 分配内存
+        last = first;
+        end_of_storage = first + sz;
+    }
+
+    ~Vector() {
+        clear();
+        alloc.deallocate(first, end_of_storage - first);
+        delete head;
+    }
+
+    Vector(const Vector<T>& rhs) {
+        copy_from(rhs);
+    }
+
+    Vector<T>& operator=(const Vector<T>& rhs) {
+        if (this != &rhs) {
+            clear();
+            alloc.deallocate(first, end_of_storage - first);
+            copy_from(rhs);
+        }
+        return *this;
+    }
+
+    void push_back(const T& val) {
+        if (full()) expand();
+        alloc.construct(last, val);
+        last++;
+    }
+
+    void pop_back() {
+        if (empty()) return;
+        verify_iterator(last - 1, last); // 检查迭代器是否失效
+        alloc.destroy(last - 1);
+        last--;
+    }
+
+    T back() const {
+        if (empty()) throw std::out_of_range("empty vector");
+        return *(last - 1);
+    }
+
+    int size() const {
+        return last - first;
+    }
+
+    bool empty() const {
+        return last == first;
+    }
+
+    bool full() const {
+        return last == end_of_storage;
+    }
+
+    void expand() {
+        int new_size = size() * 2;
+        T* new_first = alloc.allocate(new_size);
+        for (int i = 0; i < size(); i++) {
+            alloc.construct(new_first + i, first[i]);
+        }
+        clear();
+        alloc.deallocate(first, end_of_storage - first);
+        first = new_first;
+        last = first + size();
+        end_of_storage = first + new_size;
+    }
+
+    T& operator[](int n) const {
+        if (n < 0 || n >= size()) throw std::out_of_range("index out of range");
+        return first[n];
+    }
+
+    class iterator {
     public:
-        Vector(int sz = 10, Allocator allocator = Allocator()):
-            first(nullptr),
-            last(nullptr),
-            end_of_storage(nullptr),
-            alloc(allocator)
-           
-        {
-            // first = new T[sz];
-            first = alloc.allocate(sz);//分配内存
-            last = first;
-            end_of_storage = first + sz;
+        iterator(Vector<T, Allocator>* vec, T* p = nullptr)
+            : current(p), parent(vec) {}
+
+        bool operator!=(const iterator& rhs) const {
+            return current != rhs.current;
         }
-        ~Vector(){
-            // delete[] first;
-            for(T* p = first;p!=last;p++){
-                alloc.destroy(p);//把指针指向的对象调用析构函数
-            }
-            alloc.deallocate(first,end_of_storage-first);//释放内存
-            first = nullptr;
-            last = nullptr;
-            end_of_storage = nullptr;
+
+        T& operator*() const {
+            if (!current) throw std::invalid_argument("iterator not valid");
+            return *current;
         }
-        Vector(const Vector<T>& rhs){
-            int size = rhs.end_of_storage - rhs.first;
-            // first = new T[size];
-            first = alloc.allocate(size);
-            int len=rhs.last-rhs.first;
-            for(int i=0;i<len;i++){
-                // first[i]=rhs.first[i];
-                alloc.construct(first+i,rhs.first[i]);
-            }
-            last = first + len;
-            end_of_storage = first + size;
-        }
-        Vector<T>& operator=(const Vector<T>& rhs){
-            if(this!=&rhs){
-                // delete[] first;//释放原有内存
-                for(T* p = first;p!=last;p++){
-                    alloc.destroy(p);//把指针指向的对象调用析构函数
-                }
-                alloc.deallocate(first,end_of_storage-first);//释放内存
-                // int size = rhs.end_of_storage - rhs.first;
-                // first = new T[size];
-                // int len=rhs.last-rhs.first;
-                // for(int i=0;i<len;i++){
-                //     first[i]=rhs.first[i];
-                // }
-                int size = rhs.end_of_storage - rhs.first;
-                first = alloc.allocate(size);
-                int len=rhs.last-rhs.first;
-                for(int i=0;i<len;i++){
-                    // first[i]=rhs.first[i];
-                    alloc.construct(first+i,rhs.first[i]);
-                }
-                last = first + len;
-                end_of_storage = first + size;
-            }
+
+        iterator& operator++() {
+            if (!current) throw std::invalid_argument("iterator not valid");
+            current++;
             return *this;
         }
-        void push_back(const T& val)//尾插法
-        {
-           if(full())
-           expand();
-        //    *last = val;
-        //    last++;
-            alloc.construct(last,val);
-            last++;
+
+        iterator operator++(int) {
+            iterator tmp(*this);
+            ++(*this);
+            return tmp;
         }
-        void pop_back(){
-            if(empty())
-            return;
-               
-            // last--;
-            alloc.destroy(last-1);
-            last--;
-        }
-        T back() const{
-            if(empty())
-            throw std::out_of_range("empty vector");
-            return *(last-1);
-        }
-        int size() const{
-            return last - first;
-        }
-        bool empty() const{
-            return last == first;
-        }
-        bool full() const{
-            return last == end_of_storage;
-        }
-        void expand(){
-            int new_size = size() * 2;
-            // T* new_first = new T[new_size];
-            T* new_first = alloc.allocate(new_size);
-            for(int i=0;i<size();i++){
-                // new_first[i] = first[i];
-                alloc.construct(new_first+i,first[i]);
+
+        T* get_current() const { return current; }
+
+        void invalidate() { current = nullptr; } // 标记迭代器失效
+
+    private:
+        T* current;
+        Vector<T, Allocator>* parent;
+    };
+
+    iterator begin() {
+        return iterator(this, first);
+    }
+
+    iterator end() {
+        return iterator(this, last);
+    }
+
+    void verify_iterator(T* first, T* last) {
+        iterator_base* pre = head;
+        iterator_base* it = head->next;
+        while (it) {
+            if (it->cur->get_current() > first && it->cur->get_current() <= last) {
+                it->cur->invalidate(); // 调用迭代器的方法使其失效
+                pre->next = it->next;  // 移除失效迭代器
+                delete it;
+                it = pre->next;
+            } else {
+                pre = it;
+                it = it->next;
             }
-            // delete[] first;
-            for(T* p = first;p!=last;p++){
-                alloc.destroy(p);//把指针指向的对象调用析构函数
-            }
-            alloc.deallocate(first,end_of_storage-first);//释放内存
-            first = new_first;
-            last = first + size();
-            end_of_storage = first + new_size;
         }
-        T& operator[](int n) const{
-            if(n < 0 || n >= size())
-                throw std::out_of_range("index out of range");
-            return first[n];
+    }
+
+    iterator insert(iterator pos, const T& val) {
+        if (!pos.get_current()) throw std::invalid_argument("iterator not valid");
+        if (full()) expand();
+        verify_iterator(pos.get_current() - 1, last); // 检查迭代器是否失效
+        T* p = last;
+        while (p != pos.get_current()) {
+            alloc.construct(p, *(p - 1));
+            p--;
         }
-        class iterator{
-            public:
-                iterator(T* p):current(p){
-                }
-                bool operator!=(const iterator& rhs) const{
-                    return current != rhs.current;
-                }
-                T& operator*() const{
-                    return *current;
-                }
-                iterator& operator++(){
-                    current++;
-                    return *this;
-                }
-                iterator operator++(int){
-                    iterator tmp(*this);
-                    current++;
-                    return tmp;
-                }
-            private:
-                T* current;
-        };
-        iterator begin() const{
-            return iterator(first);
+        alloc.construct(p, val);
+        last++;
+        return iterator(this, p);
+    }
+    iterator erase(iterator pos) {
+        if (!pos.get_current()) throw std::invalid_argument("iterator not valid");
+        verify_iterator(pos.get_current() - 1, last); // 检查迭代器是否失效
+        T* p = pos.get_current();
+        while (p != last) {
+            alloc.destroy(p);
+            alloc.construct(p, *(p + 1));
+            p++;
         }
-        iterator end() const{
-            return iterator(last);
-        }
-        
-        
+        alloc.destroy(p);
+        last--;
+        return iterator(this, pos.get_current());
+    }
+
 private:
-   T* first;//指向数组首元素的指针
-   T* last;//指向数组尾元素的指针
-   T* end_of_storage;//指向数组尾后位置的指针
-   Allocator  alloc;//分配器
+    void clear() {
+        for (T* p = first; p != last; p++) {
+            alloc.destroy(p);
+        }
+        last = first;
+    }
+
+    void copy_from(const Vector<T>& rhs) {
+        int size = rhs.end_of_storage - rhs.first;
+        first = alloc.allocate(size);
+        int len = rhs.last - rhs.first;
+        for (int i = 0; i < len; i++) {
+            alloc.construct(first + i, rhs.first[i]);
+        }
+        last = first + len;
+        end_of_storage = first + size;
+    }
+
+    T* first;             // 指向数组首元素的指针
+    T* last;              // 指向数组尾元素的指针
+    T* end_of_storage;    // 指向数组尾后位置的指针
+    Allocator alloc;      // 分配器
+
+    struct iterator_base {
+        iterator_base(iterator* c = nullptr, iterator_base* n = nullptr)
+            : cur(c), next(n) {}
+        iterator* cur;
+        iterator_base* next;
+    };
+
+    iterator_base* head;
 };
+
 #endif
--- Vector/src/main.cpp
+++ Vector/src/main.cpp
@@ -1,51 +1,188 @@
-#include <iostream>
-#include "Vector.h" 
-class Test
-{
-private:
-    /* data */
-public:
-     Test();
-     ~Test();
-};
- Test:: Test()
-{
-    std::cout<<"Test"<<std::endl;
-}
-Test::~Test()
-{
-    std::cout<<"~Test"<<std::endl;
-}
+// #include <iostream>
+// #include "Vector.h" 
+// #include<vector> 
+// //迭代器为什么失效
+// //当容器的元素被删除后，迭代器失效，因为迭代器指向的位置已经不再是容器中的元素。
+// //当从插入元素的时候，迭代器也会失效，因为插入元素之后，容器中的元素位置发生了变化。
+// //因此，在容器中进行元素的删除、插入操作时，一定要注意迭代器的失效问题。
+// //解决方法：   
+// //对于insret来说，可以先获取插入位置的迭代器，然后再插入元素。
+// //对于erase来说，可以先获取删除位置的迭代器，然后再调用erase函数。
+// //但是，对于其他操作，比如遍历容器，或者对容器进行其他操作，都要注意迭代器的失效问题。
+// //因此，在容器中进行元素的删除、插入操作时，一定要注意迭代器的失效问题。
+// //另外，对于容器的遍历，可以用for循环，也可以用迭代器。
+// //对于for循环，可以用range-based for循环，也可以用普通的for循环。
+// //对于迭代器，可以用begin()和end()函数，也可以用其他函数。
+// //总结：
+// //1.对于insret来说，可以先获取插入位置的迭代器，然后再插入元素。
+// //2.对于erase来说，可以先获取删除位置的迭代器，然后再调用erase函数。
+// //3.对于其他操作，比如遍历容器，或者对容器进行其他操作，都要注意迭代器的失效问题。
+// //4.对于容器的遍历，可以用for循环，也可以用迭代器。
+// //5.对于for循环，可以用range-based for循环，也可以用普通的for循环。
+// //6.对于迭代器，可以用begin()和end()函数，也可以用其他函数。
+// class Test
+// {
+// private:
+//     /* data */
+// public:
+//      Test();
+//      ~Test();
+// };
+//  Test:: Test()
+// {
+//     std::cout<<"Test"<<std::endl;
+// }
+// Test::~Test()
+// {
+//     std::cout<<"~Test"<<std::endl;
+// }
 
+// int main()
+// {
+//     // // Vector<Test,Allocator<Test>> v;
+//     // Vector<int> v1(5);
+//     // v1.push_back(1);
+//     // v1.push_back(2);
+//     // v1.push_back(3);
+//     // v1.push_back(4);
+//     // v1.push_back(5);
+//     // // for(int i=0;i<v1.size();i++)
+//     // // {
+//     // //     std::cout<<v1[i]<<" ";
+//     // // }
+//     // // while(!v1.empty())
+//     // // {
+//     // //     std::cout<<v1.back()<<" ";
+//     // //     v1.pop_back();
+//     // // }
+//     // // std::cout<<std::endl;
+//     // //  for(int i=0;i<v1.size();i++)
+//     // // {
+//     // //     std::cout<<v1[i]<<" ";
+//     // // }
+//     // // std::cout<<std::endl;
+//     // for(auto it=v1.begin();it!=v1.end();it++)
+//     // {
+//     //     std::cout<<*it<<" ";
+//     // }
+//     // std::cout<<std::endl;
+//     // return 0;
+//     std::vector<int> v1;
+//     for(int i=0;i<20;i++)
+//     {
+//         v1.push_back(rand()%100+1);
+//     }
+//     for(auto c:v1)
+//     {
+//         std::cout<<c<<" ";
+//     }
+//     std::cout<<std::endl;
+//     // //把v1容器中所有偶数删除
+//     // for(auto it=v1.begin();it!=v1.end();)
+//     // {
+//     //     if(*it%2==0)
+//     //     {
+//     //         //v1.erase(it);第一次调用后迭代器失效
+//     //         it=v1.erase(it);
+//     //     }
+//     //     else
+//     //     {
+//     //         it++;
+//     //     }
+//     // }
+//     // for(auto it=v1.begin();it!=v1.end();it++)
+//     // {
+//     //     std::cout<<*it<<" ";
+//     // }
+//     // std::cout<<std::endl;
+//     // 给v1容器中所有偶数前面添加一个小于偶数值1的数
+//     // for(auto it=v1.begin();it!=v1.end();)
+//     // {
+//     //     if(*it%2==0)
+//     //     {
+//     //         if(*it==2)   //如果偶数为2，则不添加
+//     //         {
+//     //             it++;
+//     //         }
+//     //         else
+//     //         {
+//     //             it=v1.insert(it,1);
+//     //             it++;
+//     //         }
+//     //     }
+//     //     else
+//     //     {
+//     //         it++;
+//     //     }
+//     // }
+//     for(auto it=v1.begin();it!=v1.end();it++)
+//     { 
+//         if(*it%2==0)
+//         {
+//             if(*it==2)   //如果偶数为2，则不添加
+//             {
+               
+//             }
+//             else
+//             {
+//                 it=v1.insert(it,1);
+//                 it++;
+//             }
+//         }
+           
+//     }
+
+//     for(auto it=v1.begin();it!=v1.end();it++)
+//     {
+//         std::cout<<*it<<" ";
+//     }
+//     std::cout<<std::endl;
+//     return 0;
+
+    
+// }
+    
+#include<iostream>
+#include"Vector.h"
+using namespace std;
 int main()
 {
-    // Vector<Test,Allocator<Test>> v;
-    Vector<int> v1(5);
-    v1.push_back(1);
-    v1.push_back(2);
-    v1.push_back(3);
-    v1.push_back(4);
-    v1.push_back(5);
-    // for(int i=0;i<v1.size();i++)
-    // {
-    //     std::cout<<v1[i]<<" ";
-    // }
-    // while(!v1.empty())
-    // {
-    //     std::cout<<v1.back()<<" ";
-    //     v1.pop_back();
-    // }
-    // std::cout<<std::endl;
-    //  for(int i=0;i<v1.size();i++)
-    // {
-    //     std::cout<<v1[i]<<" ";
-    // }
-    // std::cout<<std::endl;
+    Vector<int> v1(50);
+    for(int i=0;i<5;i++)
+    {
+        v1.push_back(i);
+    }
     for(auto it=v1.begin();it!=v1.end();it++)
     {
-        std::cout<<*it<<" ";
+        if (*it%2==0)
+        {
+            v1.insert(it,100);
+            it++;
+        }
     }
-    std::cout<<std::endl;
+    for(auto it=v1.begin();it!=v1.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+    for(auto it=v1.begin();it!=v1.end();)
+    {
+        if(*it%2==0)
+        {
+            //v1.erase(it);第一次调用后迭代器失效
+            it=v1.erase(it);
+        }
+        else
+        {
+            it++;
+        }
+    }
+    for(auto it=v1.begin();it!=v1.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+    cout<<endl;
     return 0;
 }
-    
+
+        
